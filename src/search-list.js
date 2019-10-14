4c231348629e2c269ff764f096b392474e66f2cf
xiehongtao <531026360@qq.com>
2019-10-14 12:58:10 +0800

    完善search bar

--- src/search-list.js
+++ src/search-list.js
@@ -5,17 +5,72 @@ class SearchList extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-
+            list: [],
+            searchList: [],
+            searchText: '',
+            isOnlyProduct: false,
+            debounce: new Debounce(800)
         }
+    }
+
+    componentDidMount() {
+        this.setState({
+            list: mockData,
+            searchList: mockData
+        })
+    }
+
+    onSearch(e) {
+        this.setState({
+            searchText: e.target.value
+        })
+        this.state.debounce(() => {
+            let regexp = new RegExp(this.state.searchText, 'i');
+            let searchList = this.state.list.filter(item => {
+                console.log(regexp);
+                console.log(item.searchText);
+                console.log(regexp.test(item.searchText))
+                return regexp.test(item.name);
+            })
+            this.setState({
+                timer: null,
+                searchList
+            })
+        })
+    }
+
+    onCheck(e) {
+        this.setState({
+            isOnlyProduct: e.target.checked
+        })
     }
 
     render() {
         return (
             <div className="search-list">
-                <SearchBar />
-                <ProductList list={ mockData } />
+                <SearchBar 
+                    searchText={ this.state.searchText }
+                    isOnlyProduct={ this.state.isOnlyProduct }
+                    onSearch={ (e) => { this.onSearch(e) } } 
+                    onCheck={ (e) => { this.onCheck(e) } } 
+                />
+                <ProductList
+                    list={ this.state.searchList }
+                    searchText={ this.state.searchText }
+                    isOnlyProduct={ this.state.isOnlyProduct } 
+                />
             </div>
         );
+    }
+}
+
+function Debounce(delay) {
+    var timer = null;
+    return function(fn) {
+        if(timer !== null) clearTimeout(timer);
+        timer = setTimeout(() => {
+            if(typeof fn === 'function') fn();
+        }, delay);
     }
 }
 
@@ -24,9 +79,17 @@ class SearchBar extends React.Component {
     render() {
         return (
             <div className="search-bar">
-                <input type="text"></input>
+                <input 
+                    type="text" 
+                    value={ this.props.searchText }
+                    onChange={ this.props.onSearch } 
+                ></input>
                 <div className="search-bar-check">
-                    <input type="checkbox"></input>
+                    <input 
+                        type="checkbox"
+                        value={ this.props.isOnlyProduct } 
+                        onChange={ this.props.onCheck } 
+                    ></input>
                     <span>Only show products in list</span>
                 </div>
             </div>
@@ -46,14 +109,19 @@ function ProductList(props) {
     })
     let productItem = Object.keys(listObj).map((item, i) => {
         return (
-            <ProductItem header={item} rowList={listObj[item]} key={i} />
+            <ProductItem
+                key={i}
+                header={item}
+                rowList={listObj[item]}
+                isOnlyProduct={ props.isOnlyProduct }
+            />
         );
     })
     return (
         <div className="product-list">
             <div className="product-list-header">
                 <span className="list-column">Name</span>
-                <span className="list-column">Price</span>
+                <span className={ props.isOnlyProduct ? 'list-column hide' : 'list-column' }>Price</span>
             </div>
             { productItem }
         </div>
@@ -64,8 +132,8 @@ function ProductItem(props) {
     let productItemContent = props.rowList.map((item, i) => {
         return (
             <div className="product-item-row" key={i}>
-                <span className="list-column">{ item.name }</span>
-                <span className="list-column">{ item.price }</span>
+                <span className={ item.stocked ? 'list-column' : 'list-column no-stocked' }>{ item.name }</span>
+                <span className={ props.isOnlyProduct ? 'list-column hide' : 'list-column' }>{ item.price }</span>
             </div>
         );
     })
