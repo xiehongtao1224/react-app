08baabda440001ad81ad5a027508187a1bd49b02
xiehongtao <531026360@qq.com>
2019-10-17 20:04:50 +0800

    ref demo

--- src/ref-demo.js
+++ src/ref-demo.js
@@ -1,39 +1,79 @@
 import React from 'react';
 
-const buttonRef = React.createRef();
-
-const RefButton = React.forwardRef((props, ref) => (
-    <button ref={ref} className="ref-button" onClick={ props.buttonRefClick }>
-        {props.children}
-    </button>
-));
-
 class RefDemo extends React.Component {
     constructor(props) {
         super(props)
-        this.input1Ref = null;
-        this.input2Ref = React.createRef();
+        this.inputRef1 = null;
+        this.inputRef2 = React.createRef();
+        this.inputRef3 = null;
+        this.buttonRef = React.createRef();
+        this.componentRef = React.createRef();
     }
 
     buttonRefClick() {
-        console.log(buttonRef);
-        console.log(this.input1Ref);
-        console.log(this.input2Ref);
+        console.log(this.refs.refDiv);
+        console.log(this.buttonRef);
+        console.log(this.componentRef);
+        console.log(this.inputRef1);
+        console.log(this.inputRef2);
+        console.log(this.inputRef3);
     }
 
     render() {
         return(
             <div className="ref-demo">
-                <input ref={ (input) => {  this.input1Ref = input } }></input>
-                <input ref={ this.input2Ref }></input>
-                <RefButton 
-                    ref={ buttonRef } 
+                {/* 直接通过字符串获取 */}
+                <div ref="refDiv">refs</div>
+                {/* 通过自定义函数获取 */}
+                <input ref={ (input) => {  this.inputRef1 = input } }></input>
+                {/* 通过createRef函数获取 */}
+                <input ref={ this.inputRef2 }></input>
+                {/* 通过函数形式获取子节点ref */}
+                <InputRef inputRef={ el => {  this.inputRef3 = el } } ></InputRef>
+                {/* 通过forwardRef获取字节点ref,一般与高阶组件配合 */}
+                <ButtonRef
+                    ref={ this.buttonRef } 
                     buttonRefClick={ () => this.buttonRefClick() } 
                 >
-                    RefButton
-                </RefButton>
+                    ButtonRef
+                </ButtonRef>
+                {/* 通过ref获取整个组件 */}
+                <ComponentRef 
+                    ref={ this.componentRef }  
+                >
+                    componentRef
+                </ComponentRef>
             </div>
         )
+    }
+}
+
+function InputRef(props) {
+    return(
+        <input ref={ props.inputRef }></input>
+    );
+}
+
+const ButtonRef = React.forwardRef((props, ref) => (
+    <button ref={ref} className="button-ref" onClick={ props.buttonRefClick }>
+        {props.children}
+    </button>
+));
+
+
+class ComponentRef extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: 'componentRef'
+        }
+    }
+    render() {
+        return(
+            <div className="component-ref">
+                { this.props.children }
+            </div>
+        );
     }
 }
 
