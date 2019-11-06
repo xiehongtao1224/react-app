948db6c81697cdc0535598b72e8d3223c1808bf9
xiehongtao <531026360@qq.com>
2019-11-06 21:13:20 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,16 @@ import {
     Switch,
     Route, 
     Link,
+    NavLink,
     useRouteMatch,
-    useParams
+    useHistory,
+    useParams,
+    Prompt
 } from 'react-router-dom'
 
 class About extends React.Component{
     render() {
+        console.log(this.props);
         return <h3>About</h3>
     }
 }
@@ -21,9 +25,9 @@ function Inbox() {
     return (
         <div>
             <h2>Inbox</h2>
-            <li><Link to={`${match.url}/message/100`}>Home</Link></li>
+            <li><NavLink to={`${match.url}/message/100`}>Message</NavLink></li>
             <Switch>
-                <Route path={`${match.url}/message/:id`}>
+                <Route path={`${match.path}/message/:id`}>
                     <Message />
                 </Route>
             </Switch>
@@ -35,10 +39,30 @@ function Message (){
 
     let match = useRouteMatch();
     let params = useParams();
+    let history = useHistory();
     console.log(match)
     console.log(params);
 
-    return <h3>Message: {params.id}</h3>
+    function handleClick(e) {
+        e.preventDefault();
+        history.push('/about', { name: 'about' });
+    }
+
+    return (
+        <>
+            <Prompt
+                // when={true}
+                // message="Are you sure you want to leave?" 
+                message={location => {
+                    console.log(location)
+                    // 返回true直接跳转, false不跳转, string弹出提示
+                    return 'hello';
+                }}
+            />
+            <h3>Message: {params.id}</h3>
+            <button onClick={e => handleClick(e)}>点击跳转到about</button>
+        </>
+    )
 }
 
 class RouterApp extends React.Component{
@@ -49,19 +73,16 @@ class RouterApp extends React.Component{
                     <ul>
                         <li><Link to="/">Home</Link></li>
                         <li><Link to="/about">About</Link></li>
-                        <li><Link to="/inbox">Inbox</Link></li>
+                        <li><NavLink to="/inbox">Inbox</NavLink></li>
                     </ul>
                 </div>
                 <Switch>
-                    <Route path="/about">
-                        <About />
-                    </Route>
-                    <Route path="/inbox">
-                        <Inbox />
-                    </Route>
-                    <Route path="/">
+                    <Route exact path="/">
                         <h3>Home</h3>
                     </Route>
+                    <Route path="/about" component={About} />
+                    <Route path="/inbox" children={<Inbox />} />
+                    <Route path="*" children={<div>404 not found!</div>} />
                 </Switch>
             </Router>
         )
